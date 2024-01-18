82b69ee94efc6b8e26c9ae8c94faf7b5ce73aa3b
GoodnessJames <akomagoodness97@gmail.com>
2024-01-18 18:09:02 +0100

    Clean up repo

--- backend/src/models/init_db.py
+++
@@ -1,4 +0,0 @@
-from app import db
-from models.bug_entry_model import BugEntry
-
-db.create_all()
--- backend/src/routes/bug_entry_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from controllers.bug_entry_controller import create_bug_entry
-
-bug_entry_bp = Blueprint('bug_entry', __name__, url_prefix='/bug_entries')
-
-
-@bug_entry_bp.route('/', method=['POST'])
-def create_bug_entry_route():
-    return create_bug_entry()
