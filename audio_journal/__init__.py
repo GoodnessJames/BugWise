5c4e97c7888bd9862c33b49ec27506908ea737d4
GoodnessJames <akomagoodness97@gmail.com>
2024-01-18 18:23:34 +0100

    First codebase

---
+++ audio_journal/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e67ae8f223b0369f25088993849e8560'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from audio_journal import routes
